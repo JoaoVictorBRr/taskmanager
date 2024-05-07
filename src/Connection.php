c636db72c7b8ad206fd188a7acde224c4b988f76
João Victor de Olivera <128744729+JoaoVictorBRr@users.noreply.github.com>
2024-05-06 22:47:24 -0300

    Update Connection.php

--- src/Connection.php
+++ src/Connection.php
@@ -8,7 +8,7 @@ define("DATA_LAYER_CONFIG", [
     "port" => "3306",
     "dbname" => "taskmanager",
     "username" => "root",
-    "passwd" => "Zeka2004_",
+    "passwd" => "sua senha aqui",
     "options" => [
         PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8", //Padrão de caracteres
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, //Objeto para obter mensagem de erro
